7ad30a97dc17e00b765e8d39c932a5474dbee4da
celisa <elisa96chen@gmail.com>
2022-11-17 19:45:40 +0000

    adding tests

--- test_main.py
+++ test_main.py
@@ -1,1 +1,7 @@
 #adding tests
+from get_requests import get_activity
+
+def test_requests():
+    activity = get_activity()
+    #making sure we're receiving activities
+    assert activity['activity']
